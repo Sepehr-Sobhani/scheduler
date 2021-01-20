2e073a3129e3e010e7dcd4094d747827cc0acede
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-01-19 16:39:30 -0800

    Finalaize InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const interviewersList = props.interviewers.map((int) => {
+    return (
+      <InterviewerListItem
+        key={int.id}
+        name={int.name}
+        avatar={int.avatar}
+        selected={props.value === int.id}
+        setInterviewer={(event) => props.onChange(int.id)}
+      />
+    );
+  });
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewersList}</ul>
+    </section>
+  );
+}
