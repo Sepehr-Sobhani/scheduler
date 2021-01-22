a6ed167757b22d5f25af8e06ffa4c8f424f54a0c
Sepehr Sobhani <sobhani.sepehr@gmail.com>
2021-01-21 16:40:17 -0800

    Implement custom hook in the appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,30 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appoitment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form interviewers={[]} onCancel={back} onSave={back} />
       )}
     </article>
   );
